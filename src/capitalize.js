b6f30df0b083adaa0b5a800acd8fc2cc579c0a69
Hrybeniuk Kateryna <dp040497see@gmail.com>
2021-12-29 14:59:17 +0200

    added function implementation

--- src/capitalize.js
+++ src/capitalize.js
@@ -1,4 +1,17 @@
 function capitalize(string) {
+    let result = string.split(' ');
+    let newArray = result.map(function (item) {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+    });
+  
+    return newArray.join(' ');
+
+
+    // Еще нашла вот такой вариант реализации, мне кажется, что он намного оптимальнее.
+    //  let a = string.split(' ');
+
+    //    return str.replace(/(^|\s)\S/g, function(a) {return a.toUpperCase()});
+
 }
 
 module.exports = capitalize;
